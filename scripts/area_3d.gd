0a26fc2133501912439799fd0c6266d2059850df
bluebanana21 <m.affanfahrozi@gmail.com>
2024-09-28 22:16:08 +0700

    added-anim

--- scripts/area_3d.gd
+++ scripts/area_3d.gd
@@ -17,6 +17,7 @@ func _ready() -> void:
 func _entered_area(area_3D: Area3D) -> void:
 	if area_3D.get_parent() is CharacterBody3D:
 		area_pcam.set_priority(20)
+		print("entered room")
 
 
 func _exited_area(area_3D: Area3D) -> void:
