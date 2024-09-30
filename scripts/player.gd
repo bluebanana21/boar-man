da181edd0a3c0765a39f50fa025c86b035367241
bluebanana21 <m.affanfahrozi@gmail.com>
2024-09-30 21:00:14 +0700

    joystic-sensitivity

--- scripts/player.gd
+++ scripts/player.gd
@@ -6,6 +6,9 @@ const SPEED = 2.5
 
 var y_velo = 0
 
+
+
+
 func _ready() -> void:
 	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 
@@ -15,14 +18,17 @@ func _physics_process(delta):
 	if Input.is_action_pressed("forward"):
 		move_dir -= 1
 	if Input.is_action_pressed("backward"):
-		move_dir += 1
+		move_dir -= 1
 	if Input.is_action_pressed("right"):
 		turn_dir -= 1
 	if Input.is_action_pressed("left"):
-		turn_dir += 1
+		turn_dir -= 1
 	
-	rotation_degrees.y += turn_dir * TURN_SPEED * delta
-	velocity = global_transform.basis.z * SPEED * move_dir
+	var move_str:float = Input.get_action_strength("forward") - Input.get_action_strength("backward")
+	var turn_str = Input.get_action_strength("right") - Input.get_action_strength("left")
+	
+	rotation_degrees.y += turn_dir * TURN_SPEED * turn_str * delta
+	velocity = global_transform.basis.z * SPEED * move_str * move_dir
 	
 	move_and_slide()
 
