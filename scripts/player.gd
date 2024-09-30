5a5401b3729d2fceb69efd0b839e1219e6f26312
bluebanana21 <m.affanfahrozi@gmail.com>
2024-09-30 10:50:04 +0700

    controller_input

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,9 +1,11 @@
 extends CharacterBody3D
 
 
-const SPEED = 5.0
+const SPEED = 2.5
 const JUMP_VELOCITY = 4.5
 
+func _ready() -> void:
+	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
@@ -16,8 +18,9 @@ func _physics_process(delta: float) -> void:
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var input_dir := Input.get_vector("left", "right", "backward", "forward")
+	var input_dir := Input.get_vector("right", "left", "backward", "forward")
 	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	
 	if direction:
 		velocity.x = direction.x * SPEED
 		velocity.z = direction.z * SPEED
@@ -27,3 +30,10 @@ func _physics_process(delta: float) -> void:
 		velocity.z = move_toward(velocity.z, 0, SPEED)
 
 	move_and_slide()
+	
+
+func _input(event: InputEvent) -> void:
+	if Input.is_action_pressed("forward"):
+		rotate_y(deg_to_rad(-event.relative.x*0.5))
+	if Input.is_action_pressed("backward"):
+		rotate_y(deg_to_rad(event.relative.x*0.5))
