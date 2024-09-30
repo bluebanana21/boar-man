cf35fa6a8edff1443aba96bf8877b6df456e89b4
bluebanana21 <m.affanfahrozi@gmail.com>
2024-09-30 20:32:54 +0700

    player-rotation (improve)

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,39 +1,33 @@
 extends CharacterBody3D
 
 
+var TURN_SPEED = 180
 const SPEED = 2.5
-const JUMP_VELOCITY = 4.5
+
+var y_velo = 0
 
 func _ready() -> void:
 	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 
-func _physics_process(delta: float) -> void:
-	# Add the gravity.
-	if not is_on_floor():
-		velocity += get_gravity() * delta
-
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var input_dir := Input.get_vector("right", "left", "backward", "forward")
-	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+func _physics_process(delta):
+	var move_dir = 0
+	var turn_dir = 0
+	if Input.is_action_pressed("forward"):
+		move_dir -= 1
+	if Input.is_action_pressed("backward"):
+		move_dir += 1
+	if Input.is_action_pressed("right"):
+		turn_dir -= 1
+	if Input.is_action_pressed("left"):
+		turn_dir += 1
 	
-	if direction:
-		velocity.x = direction.x * SPEED
-		velocity.z = direction.z * SPEED
-		$AnimationPlayer.play("walking")
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		velocity.z = move_toward(velocity.z, 0, SPEED)
-
+	rotation_degrees.y += turn_dir * TURN_SPEED * delta
+	velocity = global_transform.basis.z * SPEED * move_dir
+	
 	move_and_slide()
-	
 
 func _input(event: InputEvent) -> void:
-	if Input.is_action_pressed("forward"):
-		rotate_y(deg_to_rad(-event.relative.x*0.5))
-	if Input.is_action_pressed("backward"):
-		rotate_y(deg_to_rad(event.relative.x*0.5))
+	if Input.is_action_just_pressed("attack"):
+		print("swing")
+	
+	
