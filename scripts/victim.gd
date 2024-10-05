f821ea747a1a9c8d08d1de0377e59f3ce541f008
bluebanana21 <m.affanfahrozi@gmail.com>
2024-10-05 22:27:46 +0700

    victim-text

--- scripts/victim.gd
+++ scripts/victim.gd
@@ -1,12 +1,15 @@
 extends CharacterBody3D
 
+@onready var victim_label = $Label3D
 
-const SPEED = 5.0
-const JUMP_VELOCITY = 4.5
+const SPEED: float = 5.0
+const JUMP_VELOCITY: float = 4.5
+var combo_points: int = 100
 
 
 func _physics_process(delta: float) -> void:
 	pass
 
 func damage() -> void:
+	victim_label.text = "gaming"
 	print("victim dead")
