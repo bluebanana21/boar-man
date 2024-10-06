67f096352d1abb01d23f1ea08ff15c914a5516ea
bluebanana21 <m.affanfahrozi@gmail.com>
2024-10-06 22:31:16 +0700

    damage-number

--- scripts/victim.gd
+++ scripts/victim.gd
@@ -1,7 +1,9 @@
 extends CharacterBody3D
 
 @onready var victim_label: Label3D = $VictimLabel
+@onready var label_timer: Timer = $VictimLabel/LabelTimer
 @onready var game_manager: Node = %GameManager
+@onready var animation_player: AnimationPlayer = $VictimLabel/AnimationPlayer
 
 const SPEED: float = 5.0
 const JUMP_VELOCITY: float = 4.5
@@ -15,6 +17,10 @@ func _physics_process(delta: float) -> void:
 
 func damage() -> void:
 	victim_label.visible = true
-	victim_label.text = "100"
+	victim_label.text = str(combo_points)
+	animation_player.play("appear")
 	game_manager.add_point()
 	print("victim dead")
+	
+	await animation_player.animation_finished
+	victim_label.visible = false
