2424aeb18606a38b2122b918044b02e7ea79ff17
bluebanana21 <m.affanfahrozi@gmail.com>
2024-10-06 17:22:09 +0700

    viewport-fixes

--- scripts/victim.gd
+++ scripts/victim.gd
@@ -6,10 +6,13 @@ const SPEED: float = 5.0
 const JUMP_VELOCITY: float = 4.5
 var combo_points: int = 100
 
+func _ready() -> void:
+	victim_label.visible = false
 
 func _physics_process(delta: float) -> void:
 	pass
 
 func damage() -> void:
-	victim_label.text = "gaming"
+	victim_label.visible = true
+	victim_label.text = "100"
 	print("victim dead")
