c518c62eafe825e26fa10ea01212fb6a39176124
bluebanana21 <m.affanfahrozi@gmail.com>
2024-12-29 12:54:01 +0700

    update: updates pathfinding every 30 frames in victim violent state

--- scripts/victim/States/victim_violent.gd
+++ scripts/victim/States/victim_violent.gd
@@ -7,10 +7,11 @@ class_name VictimViolent
 @onready var animation_tree: AnimationTree = $"../../AnimationTree"
 
 @export var victim:CharacterBody3D
-@export var move_speed: float = 15.0
 
+const move_speed: float = 5.0
 const attack_range:float = 1.5
 
+var new_velocity := Vector3.ZERO
 
 #Rotates the skeleton model 180 degrees, because if you 
 #dont rotate it will face the wrong direction
@@ -31,13 +32,16 @@ func Physics_update(delta: float):
 		await animation_tree.animation_finished
 		animation_tree["parameters/conditions/punch"] = 0.0
 	else:
-		victim.velocity = Vector3.ZERO
-		nav_agent.set_target_position(player.global_transform.origin)
-		var next_nav_point = nav_agent.get_next_path_position()
-		victim.velocity = (next_nav_point - victim.global_transform.origin).normalized() * move_speed
+		if Engine.get_physics_frames() % 30 == 0:
+			victim.velocity = Vector3.ZERO
+			nav_agent.set_target_position(player.global_transform.origin)
+			var next_nav_point := nav_agent.get_next_path_position()
+			var new_velocity := (next_nav_point - victim.global_transform.origin).normalized() * move_speed
+			victim.velocity = new_velocity
+			print(new_velocity)
 		
 		victim.look_at(Vector3(player.global_position.x, victim.global_position.y, player.global_position.z), Vector3.UP)
-	
+		
 		victim.move_and_slide()
 
 
@@ -47,4 +51,3 @@ func hit_finished():
 	if victim.global_position.distance_to(player.global_position) < attack_range:
 		player.hit()
 	return
-	
